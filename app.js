708f6c6051549797e82d0741bf8bb7003caab2a3
Yunil Choi <yichoi@in4ucloud.com>
2022-12-16 16:42:45 +0900

    not completed

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const nunjucks = require('nunjucks')
 const dotenv = require('dotenv')
+const ColorHash = require('color-hash').default
 
 dotenv.config()
 const webSocket = require('./socket')
 const indexRouter = require('./routes')
+const connect = require('./schemas')
 
 const app = express()
 app.set('port', process.env.PORT || 8005)
@@ -17,23 +19,32 @@ nunjucks.configure('views', {
   express: app,
   watch: true,
 })
+connect()
 
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+})
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/gif', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  }),
-)
+app.use(sessionMiddleware)
+
+app.use((req, res, next) => {
+  if (!req.session.color) {
+    const colorHash = new ColorHash()
+    req.session.color = colorHash.hex(req.sessionID)
+  }
+  next()
+})
 
 app.use('/', indexRouter)
 
@@ -54,4 +65,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중')
 })
 
-webSocket(server)
+webSocket(server, app, sessionMiddleware)
